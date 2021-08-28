1f7ba3981d6f7ef26adf992ff8201ff6d71b3c2e
USIS096618 <60713895+USIS096618@users.noreply.github.com>
2021-08-27 21:27:54 -0600

    ..

--- Config.js
+++ Config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3380,
     // db: process.env.MONGODB || 'mongodb://localhost:27017/SRP',
-    db: 'mongodb+srv://codeMaster:codeMaster@cluster0.xq7jk.mongodb.net/test'
+    db: 'mongodb+srv://nov132018:nov132018@cluster0.wunvs.mongodb.net/myFirstDatabase'
 }
